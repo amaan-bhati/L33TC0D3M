6b2ca5b9bf33354c7500ee3c119e525da4ce8d70
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-10-19 22:39:40 +0530

    Update construct-binary-tree-from-inorder-and-postorder-traversal.js

--- construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -1,1 +1,23 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+}
+
+function buildTree(inorder, postorder) {
+  if (inorder.length === 0 || postorder.length === 0) {
+    return null;
+  }
+
+  const rootValue = postorder.pop();
+  const root = new TreeNode(rootValue);
+
+  const rootIndexInorder = inorder.indexOf(rootValue);
+
+  root.right = buildTree(inorder.slice(rootIndexInorder + 1), postorder);
+  root.left = buildTree(inorder.slice(0, rootIndexInorder), postorder);
+
+  return root;
+}
 
