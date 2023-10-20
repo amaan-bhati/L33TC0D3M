fe590dcfc48444697fe7c5c2dc79ed09b5479dd4
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-10-20 23:44:29 +0530

    Update flatten-binary-tree-to-linked-list.js

--- flatten-binary-tree-to-linked-list.js
+++ flatten-binary-tree-to-linked-list.js
@@ -1,1 +1,29 @@
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+function flatten(root) {
+    if (root === null) return;
+
+    // Save the right subtree
+    let right = root.right;
+
+    // Flatten the left subtree
+    flatten(root.left);
+
+    // Attach the flattened left subtree to the right
+    root.right = root.left;
+    root.left = null;
+
+    // Find the rightmost node and attach the saved right subtree
+    let current = root;
+    while (current.right !== null) {
+        current = current.right;
+    }
+    current.right = right;
+
+    // Continue flattening the right subtree
+    flatten(right);
+}
 
