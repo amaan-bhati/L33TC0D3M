868cbeaf2494a1d43de8bfbb987f52a5850bfa14
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-10-20 23:45:34 +0530

    Update validate-binary-search-tree.js

--- validate-binary-search-tree.js
+++ validate-binary-search-tree.js
@@ -1,1 +1,17 @@
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+function isValidBST(root) {
+    function inorderTraversal(node, lower, upper) {
+        if (node === null) return true;
+
+        if (node.val <= lower || node.val >= upper) return false;
+
+        return inorderTraversal(node.left, lower, node.val) && inorderTraversal(node.right, node.val, upper);
+    }
+
+    return inorderTraversal(root, -Infinity, Infinity);
+}
 
