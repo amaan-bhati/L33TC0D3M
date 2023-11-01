2db0fdbbb6119d18f5e2bf7464a3884dc826cef8
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-11-01 23:06:20 +0530

    Update non overlapping intervals.js

--- non overlapping intervals.js
+++ non overlapping intervals.js
@@ -1,7 +1,3 @@
-/**
- * @param {number[][]} intervals
- * @return {number}
- */
 var eraseOverlapIntervals = function (intervals) {
     intervals.sort((a, b) => a[1] - b[1]);
     let end = intervals[0][1];
