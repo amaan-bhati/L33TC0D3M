546afa79aa55c4ce763d66d50e0911571e1a4e20
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-02-12 21:18:39 +0530

    Create add two numbers-2.js

---
+++ add two numbers-2.js
@@ -0,0 +1,22 @@
+
+var addTwoNumbers = function (l1, l2) {
+    const stack1 = getStack(l1);
+    const stack2 = getStack(l2);
+    let head = null
+    let carry = 0;
+    while (stack1.length || stack2.length || carry) {
+        const sum = (stack1.pop() || 0) + (stack2.pop() || 0) + carry;
+        carry = Math.floor(sum / 10);
+        head = new ListNode(sum % 10, head);
+    }
+    return head;
+};
+
+var getStack = function (node) {
+    const stack = [];
+    while (node) {
+        stack.push(node.val);
+        node = node.next;
+    }
+    return stack;
+}
