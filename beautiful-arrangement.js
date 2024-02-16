19f1644780623726f60f9cc0823376545fa2e030
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-02-16 23:59:54 +0530

    Update beautiful-arrangement.js

--- beautiful-arrangement.js
+++ beautiful-arrangement.js
@@ -1,18 +1,1 @@
-var countArrangement = function (n) {
-    let count = 0;
-    const visited = Array(n + 1).fill(false)
 
-    function permute(idx) {
-        if (idx > n) count++;
-
-        for (let i = 1; i <= n; i++) {
-            if (visited[i]) continue;
-            if (i % idx && idx % i) continue;
-            visited[i] = true;
-            permute(idx + 1);
-            visited[i] = false;
-        }
-    }
-    permute(1);
-    return count;
-};
