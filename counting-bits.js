eab46dfaa966b4a0cf1ab4aa55937b75fd93da51
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-02-20 23:32:27 +0530

    Create counting-bits.js

---
+++ counting-bits.js
@@ -0,0 +1,14 @@
+var countBits = function (n) {
+    const result = [];
+
+    for (let i = 0; i <= n; i++) {
+        result.push(
+            i
+                .toString(2)
+                .split("")
+                .filter((x) => x === "1").length
+        );
+    }
+
+    return result;
+};
